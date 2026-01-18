751fa0430a584f2803ee353f43726563fd49ffb3
LinaCop <Xiserilmao@gmail.com>
2026-01-18 15:06:11 +0500

    новая хрень 3

---
+++ siz/siz_100003.js
@@ -0,0 +1,64 @@
+// siz_100003.js
+window.SIZ_DATA = window.SIZ_DATA || {};
+
+window.SIZ_DATA["100003"] = {
+  professionCode: "100003",
+  professionName: "Авиационный механик (техник) по планеру и двигателям",
+  types: [
+    {
+      typeId: "clothes",
+      typeName: "Одежда специальная защитная",
+      items: [
+        {
+          itemId: "kostyum_maslo",
+          name: "Костюм для защиты от нефтепродуктов",
+          norm: { value: 1, unit: "шт", period: "год" }
+        }
+      ]
+    },
+    {
+      typeId: "feet",
+      typeName: "Средства защиты ног",
+      items: [
+        {
+          itemId: "obuv_maslo",
+          name: "Обувь специальная для защиты от нефтепродуктов",
+          norm: { value: 1, unit: "пара", period: "год" }
+        }
+      ]
+    },
+    {
+      typeId: "hands",
+      typeName: "Средства защиты рук",
+      items: [
+        {
+          itemId: "perchatki_maslo",
+          name: "Перчатки для защиты от нефтепродуктов",
+          norm: { value: 12, unit: "пар", period: "год" }
+        }
+      ]
+    },
+    {
+      typeId: "eyes",
+      typeName: "Средства защиты глаз",
+      items: [
+        {
+          itemId: "ochki",
+          name: "Очки защитные",
+          norm: { value: 1, unit: "шт", period: "2 года" }
+        }
+      ]
+    },
+    {
+      typeId: "head",
+      typeName: "Средства защиты головы",
+      items: [
+        {
+          itemId: "kaska",
+          name: "Каска защитная",
+          norm: { value: 1, unit: "шт", period: "2 года" }
+        }
+      ]
+    }
+  ]
+};
---
+++ siz/siz_10010.js
@@ -0,0 +1,53 @@
+// siz_10010.js
+window.SIZ_DATA = window.SIZ_DATA || {};
+
+window.SIZ_DATA["10010"] = {
+  professionCode: "10010",
+  professionName: "Авиационный техник (механик) по парашютным и аварийно-спасательным средствам",
+  types: [
+    {
+      typeId: "clothes",
+      typeName: "Одежда специальная защитная",
+      items: [
+        {
+          itemId: "kostyum_mech",
+          name: "Костюм для защиты от механических воздействий",
+          norm: { value: 1, unit: "шт", period: "год" }
+        }
+      ]
+    },
+    {
+      typeId: "feet",
+      typeName: "Средства защиты ног",
+      items: [
+        {
+          itemId: "obuv_mech",
+          name: "Обувь специальная для защиты от механических воздействий",
+          norm: { value: 1, unit: "пара", period: "год" }
+        }
+      ]
+    },
+    {
+      typeId: "hands",
+      typeName: "Средства защиты рук",
+      items: [
+        {
+          itemId: "perchatki_mech",
+          name: "Перчатки для защиты от механических воздействий",
+          norm: { value: 12, unit: "пар", period: "год" }
+        }
+      ]
+    },
+    {
+      typeId: "head",
+      typeName: "Средства защиты головы",
+      items: [
+        {
+          itemId: "kaska",
+          name: "Каска защитная",
+          norm: { value: 1, unit: "шт", period: "2 года" }
+        }
+      ]
+    }
+  ]
+};
